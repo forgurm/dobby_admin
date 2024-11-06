24b640022f33097725865fa6e7822e9d30c9cb9f
forgurm <forgurm@gmail.com>
2024-11-06 22:29:16 +0900

    인증

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+module.exports = {
+    env: {
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    },
+  };
 export default nextConfig;
